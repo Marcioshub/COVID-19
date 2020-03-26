716f859efd416e709dd85fdea246578aa3f0d9f3
Marcio <mecastillo00@gmail.com>
2020-03-26 00:32:34 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axois from "axios";
+
+// Material ui
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles(theme => ({
+  input: {
+    width: "100%",
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5)
+  },
+  card: {
+    minWidth: 300,
+    display: "inline-block",
+    margin: 5,
+    backgroundColor: "white"
+  },
+  textColor: {
+    color: "red"
+  },
+  pos: {
+    marginBottom: 12
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [states, setStates] = useState(null);
+  const [filtered, setFiltered] = useState("");
+
+  async function getData() {
+    const output = await axois.get("https://corona.lmao.ninja/states");
+    setStates(output.data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <TextField
+          id="filled-basic"
+          label="Enter State"
+          variant="filled"
+          className={classes.input}
+          onChange={e => setFiltered(e.target.value.toLowerCase())}
+        />
+
+        {states !== null
+          ? states
+              .filter(f => f.state.toLowerCase().includes(filtered))
+              .map(s => (
+                <Card className={classes.card} elevation={5} key={s.state}>
+                  <CardContent>
+                    <Typography
+                      className={classes.title}
+                      variant="h5"
+                      gutterBottom
+                    >
+                      State: {s.state}
+                    </Typography>
+                    <Typography variant="h6" color="textSecondary">
+                      Cases: {s.cases}
+                    </Typography>
+                    <Typography variant="h6" color="textSecondary">
+                      Active: {s.active}
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      className={classes.pos}
+                      color="textSecondary"
+                    >
+                      Today's Cases: {s.todayCases}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      component="p"
+                      className={classes.textColor}
+                    >
+                      Deaths: {s.deaths}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      component="p"
+                      className={classes.textColor}
+                    >
+                      Today's Deaths: {s.todayDeaths}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              ))
+          : null}
+      </Container>
     </div>
   );
 }
