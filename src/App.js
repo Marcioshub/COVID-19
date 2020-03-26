bb98345853a9773173df2d1a53746dc09cddc2eb
Marcio <mecastillo00@gmail.com>
2020-03-26 12:28:28 -0400

    added grow and helper label

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import Grow from "@material-ui/core/Grow";
 
 const useStyles = makeStyles(theme => ({
   input: {
@@ -24,7 +25,10 @@ const useStyles = makeStyles(theme => ({
     margin: 5,
     backgroundColor: "white"
   },
-  textColor: {
+  greenColor: {
+    color: "green"
+  },
+  redColor: {
     color: "red"
   },
   pos: {
@@ -52,51 +56,59 @@ function App() {
       <Container maxWidth="md">
         <TextField
           id="filled-basic"
-          label="Enter State Code (example: New York is NY)"
+          label="Enter State Code"
           variant="filled"
           className={classes.input}
           onChange={e => setFiltered(e.target.value.toLowerCase())}
+          helperText="Example: New York code is NY"
         />
 
         {states !== null
           ? states
               .filter(f => f.state.toLowerCase().includes(filtered))
               .map(s => (
-                <Card className={classes.card} elevation={5} key={s.state}>
-                  <CardContent>
-                    <Typography
-                      className={classes.title}
-                      variant="h5"
-                      gutterBottom
-                    >
-                      State: {s.state}
-                    </Typography>
-                    <Typography variant="h6" color="textSecondary">
-                      Cases: {s.positive}
-                    </Typography>
+                <Grow in={true} key={s.state}>
+                  <Card className={classes.card} elevation={5}>
+                    <CardContent>
+                      <Typography
+                        className={classes.title}
+                        variant="h5"
+                        gutterBottom
+                      >
+                        State: {s.state}
+                      </Typography>
+                      <Typography variant="h6" color="textSecondary">
+                        Cases: {s.positive || 0}
+                      </Typography>
 
-                    <Typography variant="body1" component="p">
-                      Hospitalized: {s.hospitalized || 0}
-                    </Typography>
+                      <Typography
+                        variant="body1"
+                        component="p"
+                        className={classes.greenColor}
+                      >
+                        Hospitalized: {s.hospitalized || 0}
+                      </Typography>
 
-                    <Typography
-                      variant="body1"
-                      component="p"
-                      className={classes.textColor}
-                      gutterBottom
-                    >
-                      Deaths: {s.death}
-                    </Typography>
+                      <Typography
+                        variant="body1"
+                        component="p"
+                        className={classes.redColor}
+                      >
+                        Deaths: {s.death || 0}
+                      </Typography>
 
-                    <Typography
-                      variant="body2"
-                      component="p"
-                      color="textSecondary"
-                    >
-                      {moment(s.dateModified).format("MMMM Do YYYY, h:mm:ss a")}
-                    </Typography>
-                  </CardContent>
-                </Card>
+                      <Typography
+                        variant="body2"
+                        component="p"
+                        color="textSecondary"
+                      >
+                        {moment(s.dateModified).format(
+                          "MMMM Do YYYY, h:mm:ss a"
+                        )}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grow>
               ))
           : null}
       </Container>
